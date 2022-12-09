9e954e9d864a53c3497c29cd6d14937f3051cf6c
Alex Mironov <alexander.mironov@zuhlke.com>
2022-12-09 11:55:23 +0000

    Added day 9 part 2 solution

--- day09.py
+++ day09.py
@@ -18,42 +18,79 @@ class Position:
 
 
 def day09_part1(filename: str) -> int:
+    visited = simulate_rope(filename, length=1)
+    return len(visited)
+
+
+def day09_part2(filename: str) -> int:
+    visited = simulate_rope(filename, length=9)
+    return len(visited)
+
+
+def print_field(head_x, head_y, knots_x, knots_y, size=6):
+    for y in range(size - 1, -1, -1):
+        for x in range(size):
+            char = "."
+            if x == head_x and y == head_y:
+                char = "H"
+            else:
+                for i in range(len(knots_x)):
+                    if x == knots_x[i] and y == knots_y[i]:
+                        char = str(i + 1)
+                        break
+            print(char, end="")
+        print()
+    print()
+
+
+def simulate_rope(filename: str, length: int):
     lines = read_stripped_lines(filename)
-
-    head_x, head_y, tail_x, tail_y = 0, 0, 0, 0
-
+    head_x, head_y = 0, 0
+    knots_x = [0 for _ in range(length)]
+    knots_y = [0 for _ in range(length)]
     offset = {Direction.UP: (0, 1), Direction.DOWN: (0, -1), Direction.LEFT: (-1, 0), Direction.RIGHT: (1, 0)}
-
     visited = set()
-    visited.add(Position(tail_x, tail_y))
-
+    visited.add(Position(0, 0))
     for line in lines:
         direction, value = line.split(" ")
         direction = Direction(direction)
         value = int(value)
-        for i in range(value):
+        for step in range(value):
             offset_x, offset_y = offset[direction]
             head_x += offset_x
             head_y += offset_y
 
-            if abs(head_x - tail_x) <= 1 and abs(head_y - tail_y) <= 1:
-                continue
-            if (abs(head_x - tail_x) == 2 and head_y == tail_y) or \
-                    (abs(head_y - tail_y) == 2 and head_x == tail_x):
-                tail_x = (head_x + tail_x) // 2
-                tail_y = (head_y + tail_y) // 2
-            else:
-                if abs(head_x - tail_x) == 2:
-                    tail_x = (head_x + tail_x) // 2
-                    tail_y = head_y
-                if abs(head_y - tail_y) == 2:
-                    tail_y = (head_y + tail_y) // 2
-                    tail_x = head_x
-            visited.add(Position(tail_x, tail_y))
-
-    return len(visited)
+            prev_knot_x = head_x
+            prev_knot_y = head_y
+            for i in range(length):
+                knot_x = knots_x[i]
+                knot_y = knots_y[i]
+                if abs(prev_knot_x - knot_x) <= 1 and abs(prev_knot_y - knot_y) <= 1:
+                    pass
+                elif (abs(prev_knot_x - knot_x) == 2 and prev_knot_y == knot_y) or \
+                        (abs(prev_knot_y - knot_y) == 2 and prev_knot_x == knot_x):
+                    knot_x = (prev_knot_x + knot_x) // 2
+                    knot_y = (prev_knot_y + knot_y) // 2
+                else:  # diagonal
+                    offset_x = max(-1, min(1, prev_knot_x - knot_x))
+                    offset_y = max(-1, min(1, prev_knot_y - knot_y))
+                    knot_x += offset_x
+                    knot_y += offset_y
+                knots_x[i] = knot_x
+                knots_y[i] = knot_y
+                prev_knot_x = knot_x
+                prev_knot_y = knot_y
+            visited.add(Position(knots_x[-1], knots_y[-1]))
+            # print(line + " step " + str(step + 1))
+            # print_field(head_x, head_y, knots_x, knots_y, size=10)
+    return visited
 
 
 if __name__ == '__main__':
-    assert day09_part1('res/day09_sample.txt') == 13
+    assert day09_part1('res/day09_sample01.txt') == 13
     print(day09_part1('res/day09.txt'))
+    assert day09_part1('res/day09.txt') == 6339
+    assert day09_part2('res/day09_sample01.txt') == 1
+    assert day09_part2('res/day09_sample02.txt') == 36
+    print(day09_part2('res/day09.txt'))
+    assert day09_part2('res/day09.txt') == 2541
